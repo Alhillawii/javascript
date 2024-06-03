cea33e034eb7c3e682b5d04e21631f2005f8c7dd
Alhillawii <ayahhillawi9@gmail.com>
2024-06-03 14:49:12 +0300

    Message

---
+++ main.js
@@ -0,0 +1,140 @@
+let cash = 1000, current_liabilities = 500;
+console.log(cash/current_liabilities);
+
+let revenues = 1000, expenses = 500;
+console.log(revenues - expenses);
+
+let laibilities = 1000, equity = 500;
+console.log(laibilities + equity);
+
+let profit = 1000, sales = 500;
+console.log(profit * sales);
+
+//sample input: numbers: 7, 9, 2
+let x = 9;
+let y = 7;
+let z = 2;
+
+let sum = x+y+z;
+
+let avarage = sum/3;
+
+console.log(avarage);
+
+//discount 
+
+let price = 150;
+let discount = 30/100;
+
+let totaldiscount = price*discount;
+console.log(totaldiscount);
+
+let newprice = price-totaldiscount;
+console.log(newprice);
+
+//age
+
+let age = 20;
+
+if (18<age  && age< 30) {
+  console.log("true");
+} else {
+  console.log("false ");
+}
+
+//exponential
+
+let number1 = 2;
+let number2 = 3;
+
+let exponential=number1**number2;
+console.log(exponential);
+
+//remainder
+
+let num1 = 10;
+let num2 = 4;
+
+let remainder = num1 % num2;
+console.log(remainder);
+
+//task2//
+
+console.log(typeof(100));
+console.log(typeof(73.9));
+console.log(typeof(NaN));
+console.log(typeof("Water"));
+console.log(typeof(9 != 11));
+console.log("Orang" + "e");
+console.log("Orange" - "s");
+console.log("4" + "8");
+console.log("4" - "8");
+console.log("name" + 3);
+console.log("name" - 3);
+console.log(82 * "word");
+console.log(1 + "hello");
+console.log("hello" + 1);
+console.log(1 + true);
+console.log("hello" + true);
+console.log(typeof (Infinity) );
+console.log(1 == '1');
+console.log(1 === '1');
+
+//task3//
+let text = "Welcome to Orange"
+ console.log(text.toUpperCase());
+
+
+
+
+
+
+
+
+ //task4//
+ window.alert("welcome");
+
+ //task7//
+
+ let a = 10;
+ let b = 5;
+
+ console.log(a + b);
+ console.log(a - b);
+ console.log(a * b);
+ console.log(a / b);
+ console.log(a % b);
+ console.log(a > b);
+ console.log(a < b);
+ console.log(a == b);
+ console.log(a != b);
+ console.log(a >= b);
+ console.log(a <= b);
+ console.log(a += 5);
+ console.log(a -= 5);
+ console.log(a *= 2);
+ 
+ 
+ 
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
